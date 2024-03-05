584b3ed857f54a01e5413828a63166cc54722a78
frankcwl <frankcwl@126.com>
2024-03-05 16:29:04 +0800

    add new secretary ship data

--- develop.js
+++ develop.js
@@ -127,24 +127,44 @@ for (let i = 0; i < 4; ++i) {
   developList[i][2] = developList[i][0]; //油钢池实际相同
 }
 
-//fuel, ammo, steel, baux 油，彈，鋼，鋁
-//secretary 秘書艦開發池 0炮戰系 1水雷系 2航母系
-//layer 資源開發池 0油 1彈 2鋼 3鋁
-//isitaly 特殊秘书舰补正
+// fuel, ammo, steel, baux 油，彈，鋼，鋁
+// secretary 秘書艦開發池 0炮戰系 1水雷系 2航母系 3潛水系
+// layer 資源開發池 0油 1彈 2鋼 3鋁
+// isitaly 特殊秘书舰补正
 // 100 Maestrale/Grecale/Libeccio/Scirocco 及其改造形态
 // 101 Littorio(Italia)/Roma/Zara/Pola 及其改造形态
 // 102 伊势改/伊势改二
 // 103 日向改/日向改二
 // 104 Ark Royal 及其改造形态
-// 105 Warspite/Nelson 及其改造形态
+// 105 Warspite及其改造形态
 // 106 Commandant Teste 及其改造形态
 // 107 Z1/Z3 及其改造形态
 // 108 秋月/照月/凉月/初月 及其改造形态
 // 109 阿贺野/能代/矢矧/酒匂 及其改造形态
 // 110 Saratoga/Hornet 及其改造形态
-//hqlevel 司令部等級
+// 111 比叡改二/比叡改二丙
+// 112 神通改二
+// 113 神風級
+// 114 睦月級
+// 115 吹雪級
+// 116 あきつ丸/神州丸/熊野丸 及其改造形态/宗谷(特务舰)
+// 117 Nelson/Rodney 及其改造形态
+// 118 Northampton 及其改造形态
+// 119 金刚级战列舰
+// 120 Nevada 及其改造形态
+// 121 驱逐舰
+// 122 川内改二
+// 123 Helena 及其改造形态
+// 124 大淀 及其改造形态
+// 125 天津风 及其改造形态
+// 126 宗谷(灯台补给)/宗谷(南极观测)
+// 127 山汐丸 及其改造形态
+// 128 Scamp/Salmon及其改造形态
+// 129 潜水母舰
+// hqlevel 司令部等級
 
 function develop(fuel, ammo, steel, baux, secretary, isitaly, hqlevel) {
+  console.log("fuel: " + fuel + "\nammo: " + ammo + "\nsteel: " + steel + "\nbaux: " + baux + "\nsecretary: " + secretary + "\nisitaly: " + isitaly + "\nhqlevel: " + hqlevel);
   fuel = parseInt(fuel);
   ammo = parseInt(ammo);
   steel = parseInt(steel);
@@ -200,11 +220,11 @@ function develop(fuel, ammo, steel, baux, secretary, isitaly, hqlevel) {
       }
     } else if (layer == 3) {
       for (let i = 0; i < list.length; ++i) {
-        if (list[i][0] == 25) list[i][1] -= 2;
         if (list[i][0] == 16) list[i][1] -= 2;
+        if (list[i][0] == 25) list[i][1] -= 2;
         if (list[i][0] == 44) list[i][1] -= 2;
-        if (list[i][0] == 163) list[i][1] += 2;
         if (list[i][0] == 147) list[i][1] += 4;
+        if (list[i][0] == 163) list[i][1] += 2;
       }
     }
   }
@@ -260,7 +280,7 @@ function develop(fuel, ammo, steel, baux, secretary, isitaly, hqlevel) {
       }
     }
   }
-  // 105 Warspite/Nelson 炮战·铝池
+  // 105 Warspite 炮战·铝池
   if (isitaly == 105 && secretary == 0 && layer == 3) {
     for (let i = 0; i < list.length; ++i) {
       if (list[i][0] == 10) list[i][1] -= 2;
@@ -309,7 +329,7 @@ function develop(fuel, ammo, steel, baux, secretary, isitaly, hqlevel) {
       }
     }
   }
-  // 108 秋月/照月/凉月/初月 水雷
+  // 108 秋月/照月/凉月/初月/冬月 水雷
   if (isitaly == 108 && secretary == 1) {
     if (layer == 0 || layer == 2) {
       for (let i = 0; i < list.length; ++i) {
@@ -321,8 +341,8 @@ function develop(fuel, ammo, steel, baux, secretary, isitaly, hqlevel) {
       for (let i = 0; i < list.length; ++i) {
         if (list[i][0] == 20) list[i][1] -= 2;
         if (list[i][0] == 25) list[i][1] -= 2;
-        if (list[i][0] == 46) list[i][1] -= 2;
         if (list[i][0] == 30) list[i][1] += 6;
+        if (list[i][0] == 46) list[i][1] -= 2;
       }
     }
   }
@@ -360,16 +380,16 @@ function develop(fuel, ammo, steel, baux, secretary, isitaly, hqlevel) {
     } else if (layer == 3) {
       for (let i = 0; i < list.length; ++i) {
         if (list[i][0] == 18) list[i][1] -= 2;
-        if (list[i][0] == 23) list[i][1] -= 2;
-        if (list[i][0] == 26) list[i][1] -= 2;
         if (list[i][0] == 19) list[i][1] -= 2;
         if (list[i][0] == 20) list[i][1] -= 4;
+        if (list[i][0] == 23) list[i][1] -= 2;
+        if (list[i][0] == 26) list[i][1] -= 2;
         if (list[i][0] == 195) list[i][1] += 6;
         if (list[i][0] == 197) list[i][1] += 6;
       }
     }
   }
-  //比叡改二/比叡改二丙
+  // 111 比叡改二/比叡改二丙
   if (isitaly == 111 && secretary == 0) {
     if (layer == 3) {
       for (let i = 0; i < list.length; ++i) {
@@ -380,7 +400,7 @@ function develop(fuel, ammo, steel, baux, secretary, isitaly, hqlevel) {
       }
     }
   }
-  //神通改二
+  // 112 神通改二
   if (isitaly == 112 && secretary == 1) {
     if (layer == 3) {
       for (let i = 0; i < list.length; ++i) {
@@ -391,14 +411,14 @@ function develop(fuel, ammo, steel, baux, secretary, isitaly, hqlevel) {
       }
     }
   }
-  //神风
+  // 113 神风
   if (isitaly == 113 && secretary == 1) {
     if (layer == 0 || layer == 2) {
       for (let i = 0; i < list.length; ++i) {
         if (list[i][0] == 3) list[i][1] -= 2;
         if (list[i][0] == 12) list[i][1] -= 2;
-        if (list[i][0] == 19) list[i][1] -= 2;
         if (list[i][0] == 16) list[i][1] -= 2;
+        if (list[i][0] == 19) list[i][1] -= 2;
         if (list[i][0] == 37) list[i][1] += 8;
       }
     } else if (layer == 1) {
@@ -408,7 +428,7 @@ function develop(fuel, ammo, steel, baux, secretary, isitaly, hqlevel) {
       }
     }
   }
-  //睦月
+  // 114 睦月
   if (isitaly == 114 && secretary == 1) {
     if (layer == 0 || layer == 2) {
       for (let i = 0; i < list.length; ++i) {
@@ -416,94 +436,333 @@ function develop(fuel, ammo, steel, baux, secretary, isitaly, hqlevel) {
         if (list[i][0] == 11) list[i][1] -= 2;
         if (list[i][0] == 20) list[i][1] -= 2;
         if (list[i][0] == 23) list[i][1] -= 2;
-        if (list[i][0] == 65) list[i][1] -= 2;
         if (list[i][0] == 38) list[i][1] += 10;
+        if (list[i][0] == 65) list[i][1] -= 2;
       }
     } else if (layer == 1) {
       for (let i = 0; i < list.length; ++i) {
         if (list[i][0] == 5) list[i][1] -= 2;
         if (list[i][0] == 11) list[i][1] -= 2;
-        if (list[i][0] == 65) list[i][1] -= 2;
         if (list[i][0] == 38) list[i][1] += 6;
+        if (list[i][0] == 65) list[i][1] -= 2;
       }
     }
   }
-  //吹雪
+  // 115 吹雪
   if (isitaly == 115 && secretary == 1) {
     if (layer == 0 || layer == 2) {
       for (let i = 0; i < list.length; ++i) {
         if (list[i][0] == 4) list[i][1] -= 2;
         if (list[i][0] == 11) list[i][1] -= 4;
         if (list[i][0] == 25) list[i][1] -= 2;
-        if (list[i][0] == 65) list[i][1] -= 2;
         if (list[i][0] == 49) list[i][1] += 10;
+        if (list[i][0] == 65) list[i][1] -= 2;
       }
     } else if (layer == 1) {
       for (let i = 0; i < list.length; ++i) {
-        if (list[i][0] == 65) list[i][1] -= 2;
         if (list[i][0] == 11) list[i][1] -= 2;
         if (list[i][0] == 49) list[i][1] += 4;
+        if (list[i][0] == 65) list[i][1] -= 2;
       }
     }
   }
 
-  //あきつ丸,神州丸,宗谷特务舰
+  // 116 あきつ丸,神州丸,宗谷特务舰
   if (isitaly == 116 && secretary == 2) {
     if (layer == 0 || layer == 2) {
       for (let i = 0; i < list.length; ++i) {
-        if (list[i][0] == 68) list[i][1] += 4;
-        if (list[i][0] == 37) list[i][1] += 4;
-        if (list[i][0] == 38) list[i][1] += 4;
-        if (list[i][0] == 69) list[i][1] += 4;
-        if (list[i][0] == 70) list[i][1] += 2;
-        if (list[i][0] == 221) list[i][1] += 2;
-        if (list[i][0] == 445) list[i][1] += 4;
+        if (list[i][0] == 3) list[i][1] -= 2;
+        if (list[i][0] == 10) list[i][1] -= 2;
+        if (list[i][0] == 20) list[i][1] -= 2;
         if (list[i][0] == 21) list[i][1] -= 2;
-        if (list[i][0] == 181) list[i][1] -= 2;
         if (list[i][0] == 25) list[i][1] -= 6;
         if (list[i][0] == 26) list[i][1] -= 2;
-        if (list[i][0] == 41) list[i][1] -= 6;
+        if (list[i][0] == 29) list[i][1] -= 2;
         if (list[i][0] == 33) list[i][1] -= 2;
         if (list[i][0] == 34) list[i][1] -= 2;
-        if (list[i][0] == 29) list[i][1] -= 2;
+        if (list[i][0] == 37) list[i][1] += 4;
+        if (list[i][0] == 38) list[i][1] += 4;
+        if (list[i][0] == 41) list[i][1] -= 6;
+        if (list[i][0] == 68) list[i][1] += 8;
+        if (list[i][0] == 69) list[i][1] += 4;
+        if (list[i][0] == 70) list[i][1] += 2;
+        if (list[i][0] == 181) list[i][1] -= 2;
+        if (list[i][0] == 221) list[i][1] += 4;
+        if (list[i][0] == 445) list[i][1] += 4;
       }
     } else if (layer == 1) {
       for (let i = 0; i < list.length; ++i) {
-        if (list[i][0] == 68) list[i][1] += 2;
-        if (list[i][0] == 37) list[i][1] += 4;
-        if (list[i][0] == 38) list[i][1] += 4;
-        if (list[i][0] == 69) list[i][1] += 2;
-        if (list[i][0] == 70) list[i][1] += 2;
-        if (list[i][0] == 221) list[i][1] += 2;
-        if (list[i][0] == 445) list[i][1] += 6;
+        if (list[i][0] == 3) list[i][1] -= 2;
         if (list[i][0] == 17) list[i][1] -= 4;
-        if (list[i][0] == 52) list[i][1] -= 2;
-        if (list[i][0] == 57) list[i][1] -= 2;
-        if (list[i][0] == 60) list[i][1] -= 2;
         if (list[i][0] == 25) list[i][1] -= 4;
         if (list[i][0] == 26) list[i][1] -= 2;
         if (list[i][0] == 35) list[i][1] -= 4;
+        if (list[i][0] == 37) list[i][1] += 4;
+        if (list[i][0] == 38) list[i][1] += 4;
         if (list[i][0] == 41) list[i][1] -= 2;
+        if (list[i][0] == 52) list[i][1] -= 2;
+        if (list[i][0] == 57) list[i][1] -= 2;
+        if (list[i][0] == 60) list[i][1] -= 2;
+        if (list[i][0] == 68) list[i][1] += 2;
+        if (list[i][0] == 69) list[i][1] += 2;
+        if (list[i][0] == 70) list[i][1] += 2;
+        if (list[i][0] == 221) list[i][1] += 8;
+        if (list[i][0] == 445) list[i][1] += 4;
       }
     } else if (layer == 3) {
       for (let i = 0; i < list.length; ++i) {
-        if (list[i][0] == 37) list[i][1] += 2;
-        if (list[i][0] == 38) list[i][1] += 2;
-        if (list[i][0] == 69) list[i][1] += 4;
-        if (list[i][0] == 70) list[i][1] += 4;
-        if (list[i][0] == 221) list[i][1] += 6;
-        if (list[i][0] == 445) list[i][1] += 10;
         if (list[i][0] == 17) list[i][1] -= 4;
         if (list[i][0] == 22) list[i][1] -= 2;
         if (list[i][0] == 25) list[i][1] -= 2;
-        if (list[i][0] == 26) list[i][1] -= 2;
+        if (list[i][0] == 26) list[i][1] -= 4;
+        if (list[i][0] == 35) list[i][1] -= 4;
+        if (list[i][0] == 37) list[i][1] += 2;
+        if (list[i][0] == 38) list[i][1] += 2;
         if (list[i][0] == 52) list[i][1] -= 2;
         if (list[i][0] == 54) list[i][1] -= 4;
         if (list[i][0] == 55) list[i][1] -= 2;
         if (list[i][0] == 57) list[i][1] -= 2;
         if (list[i][0] == 60) list[i][1] -= 2;
         if (list[i][0] == 61) list[i][1] -= 2;
-        if (list[i][0] == 35) list[i][1] -= 4;
+        if (list[i][0] == 69) list[i][1] += 4;
+        if (list[i][0] == 70) list[i][1] += 4;
+        if (list[i][0] == 221) list[i][1] += 8;
+        if (list[i][0] == 445) list[i][1] += 12;
+      }
+    }
+  }
+  // 117 Nelson/Rodney 及其改造形态
+  if (isitaly == 117 && secretary == 0) {
+    if (layer == 0 || layer == 2) {
+      for (let i = 0; i < list.length; ++i) {
+        if (list[i][0] == 6) list[i][1] -= 2;
+        if (list[i][0] == 11) list[i][1] -= 2;
+        if (list[i][0] == 13) list[i][1] -= 2;
+        if (list[i][0] == 14) list[i][1] -= 2;
+        if (list[i][0] == 298) list[i][1] += 4;
+        if (list[i][0] == 301) list[i][1] += 4;
+      }
+    } else if (layer == 1) {
+      for (let i = 0; i < list.length; ++i) {
+        if (list[i][0] == 6) list[i][1] -= 2;
+        if (list[i][0] == 7) list[i][1] -= 2;
+        if (list[i][0] == 8) list[i][1] -= 2;
+        if (list[i][0] == 9) list[i][1] -= 2;
+        if (list[i][0] == 11) list[i][1] -= 2;
+        if (list[i][0] == 13) list[i][1] -= 2;
+        if (list[i][0] == 14) list[i][1] -= 2;
+        if (list[i][0] == 298) list[i][1] += 8;
+        if (list[i][0] == 301) list[i][1] += 6;
+      }
+    } else if (layer == 3) {
+      for (let i = 0; i < list.length; ++i) {
+        if (list[i][0] == 7) list[i][1] -= 2;
+        if (list[i][0] == 8) list[i][1] -= 2;
+        if (list[i][0] == 9) list[i][1] -= 2;
+        if (list[i][0] == 10) list[i][1] -= 2;
+        if (list[i][0] == 20) list[i][1] -= 2;
+        if (list[i][0] == 21) list[i][1] -= 2;
+        if (list[i][0] == 250) list[i][1] += 6;
+        if (list[i][0] == 298) list[i][1] += 4;
+        if (list[i][0] == 301) list[i][1] += 2;
+      }
+    }
+  }
+  // 118 Northampton 及其改造形态
+  if (isitaly == 118 && secretary == 0) {
+    if (layer == 3) {
+      for (let i = 0; i < list.length; ++i) {
+        if (list[i][0] == 20) list[i][1] -= 2;
+        if (list[i][0] == 25) list[i][1] -= 2;
+        if (list[i][0] == 414) list[i][1] += 4;
+      }
+    }
+  }
+  // 119 金刚级战列舰
+  if (isitaly == 119 && secretary == 0) {
+    if (layer == 0 || layer == 2) {
+      for (let i = 0; i < list.length; ++i) {
+        if (list[i][0] == 6) list[i][1] -= 2;
+        if (list[i][0] == 35) list[i][1] += 2;
+      }
+    } else if (layer == 1) {
+      for (let i = 0; i < list.length; ++i) {
+        if (list[i][0] == 4) list[i][1] -= 2;
+        if (list[i][0] == 6) list[i][1] -= 2;
+        if (list[i][0] == 35) list[i][1] += 6;
+        if (list[i][0] == 41) list[i][1] -= 2;
+      }
+    } else if (layer == 3) {
+      for (let i = 0; i < list.length; ++i) {
+        if (list[i][0] == 4) list[i][1] -= 2;
+        if (list[i][0] == 35) list[i][1] += 4;
+        if (list[i][0] == 41) list[i][1] -= 2;
+      }
+    }
+  }
+  // 120 Nevada 及其改造形态
+  if (isitaly == 120 && secretary == 0) {
+    if (layer == 0 || layer == 2) {
+      for (let i = 0; i < list.length; ++i) {
+        if (list[i][0] == 14) list[i][1] -= 2;
+        if (list[i][0] == 507) list[i][1] += 2;
+      }
+    } else if (layer == 1) {
+      for (let i = 0; i < list.length; ++i) {
+        if (list[i][0] == 14) list[i][1] -= 2;
+        if (list[i][0] == 507) list[i][1] += 4;
+      }
+    }
+  }
+  // 121 驱逐舰
+  if (isitaly == 121 && secretary == 1) {
+    if (layer == 0 || layer == 2) {
+      for (let i = 0; i < list.length; ++i) {
+        if (list[i][0] == 66) list[i][1] -= 2;
+        if (list[i][0] == 500) list[i][1] += 2;
+      }
+    } else if (layer == 3) {
+      for (let i = 0; i < list.length; ++i) {
+        if (list[i][0] == 24) list[i][1] -= 2;
+        if (list[i][0] == 500) list[i][1] += 2;
+      }
+    }
+  }
+  // 122 川内改二
+  if (isitaly == 122 && secretary == 1) {
+    if (layer == 3) {
+      for (let i = 0; i < list.length; ++i) {
+        if (list[i][0] == 20) list[i][1] -= 2;
+        if (list[i][0] == 102) list[i][1] += 2;
+      }
+    }
+  }
+  // 123 Helena 及其改造形态
+  if (isitaly == 123 && secretary == 1) {
+    if (layer == 3) {
+      for (let i = 0; i < list.length; ++i) {
+        if (list[i][0] == 46) list[i][1] -= 4;
+        if (list[i][0] == 414) list[i][1] += 4;
+      }
+    }
+  }
+  // 124 大淀 及其改造形态
+  if (isitaly == 124 && secretary == 1) {
+    if (layer == 0 || layer == 2) {
+      for (let i = 0; i < list.length; ++i) {
+        if (list[i][0] == 44) list[i][1] -= 2;
+        if (list[i][0] == 71) list[i][1] += 2;
+      }
+    } else if (layer == 1) {
+      for (let i = 0; i < list.length; ++i) {
+        if (list[i][0] == 23) list[i][1] -= 2;
+        if (list[i][0] == 44) list[i][1] -= 4;
+        if (list[i][0] == 71) list[i][1] += 6;
+      }
+    } else if (layer == 3) {
+      for (let i = 0; i < list.length; ++i) {
+        if (list[i][0] == 23) list[i][1] -= 2;
+        if (list[i][0] == 44) list[i][1] -= 2;
+        if (list[i][0] == 71) list[i][1] += 4;
+      }
+    }
+  }
+  // 125 天津风 及其改造形态
+  if (isitaly == 125 && secretary == 1) {
+    if (layer == 0 || layer == 2) {
+      for (let i = 0; i < list.length; ++i) {
+        if (list[i][0] == 1) list[i][1] -= 2;
+        if (list[i][0] == 34) list[i][1] += 6;
+        if (list[i][0] == 66) list[i][1] -= 2;
+        if (list[i][0] == 75) list[i][1] -= 2;
+      }
+    }
+  }
+  // 126 宗谷(灯台补给)/宗谷(南极观测)
+  if (isitaly == 126 && secretary == 1) {
+    if (layer == 1) {
+      for (let i = 0; i < list.length; ++i) {
+        if (list[i][0] == 24) list[i][1] -= 2;
+        if (list[i][0] == 69) list[i][1] += 2;
+      }
+    } else if (layer == 3) {
+      for (let i = 0; i < list.length; ++i) {
+        if (list[i][0] == 24) list[i][1] -= 2;
+        if (list[i][0] == 69) list[i][1] += 4;
+        if (list[i][0] == 181) list[i][1] -= 2;
+      }
+    }
+  }
+  // 127 山汐丸 及其改造形态
+  if (isitaly == 127 && secretary == 1) {
+    if (layer == 1) {
+      for (let i = 0; i < list.length; ++i) {
+        if (list[i][0] == 10) list[i][1] -= 2;
+        if (list[i][0] == 17) list[i][1] -= 2;
+        if (list[i][0] == 24) list[i][1] -= 2;
+        if (list[i][0] == 69) list[i][1] += 2;
+        if (list[i][0] == 70) list[i][1] += 2;
+        if (list[i][0] == 445) list[i][1] += 2;
+      }
+    } else if (layer == 3) {
+      for (let i = 0; i < list.length; ++i) {
+        if (list[i][0] == 10) list[i][1] -= 2;
+        if (list[i][0] == 17) list[i][1] -= 2;
+        if (list[i][0] == 21) list[i][1] -= 2;
+        if (list[i][0] == 24) list[i][1] -= 2;
+        if (list[i][0] == 25) list[i][1] -= 2;
+        if (list[i][0] == 69) list[i][1] += 4;
+        if (list[i][0] == 70) list[i][1] += 6;
+        if (list[i][0] == 120) list[i][1] -= 2;
+        if (list[i][0] == 181) list[i][1] -= 2;
+        if (list[i][0] == 445) list[i][1] += 4;
+      }
+    }
+  }
+  // 128 Scamp/Salmon及其改造形态
+  if (isitaly == 128 && secretary == 3) {
+    if (layer == 0 || layer == 2) {
+      for (let i = 0; i < list.length; ++i) {
+        if (list[i][0] == 75) list[i][1] -= 4;
+        if (list[i][0] == 213) list[i][1] -= 2;
+        if (list[i][0] == 440) list[i][1] += 4;
+        if (list[i][0] == 442) list[i][1] += 4;
+        if (list[i][0] == 457) list[i][1] -= 2;
+        if (list[i][0] == 511) list[i][1] += 4;
+      }
+    } else if (layer == 1) {
+      for (let i = 0; i < list.length; ++i) {
+        if (list[i][0] == 75) list[i][1] -= 4;
+        if (list[i][0] == 213) list[i][1] -= 6;
+        if (list[i][0] == 440) list[i][1] += 8;
+        if (list[i][0] == 457) list[i][1] -= 4;
+        if (list[i][0] == 511) list[i][1] += 4;
+      }
+    } else if (layer == 3) {
+      for (let i = 0; i < list.length; ++i) {
+        if (list[i][0] == 75) list[i][1] -= 2;
+        if (list[i][0] == 213) list[i][1] -= 4;
+        if (list[i][0] == 440) list[i][1] += 4;
+        if (list[i][0] == 457) list[i][1] -= 2;
+      }
+    }
+  }
+  // 129 潜水母舰
+  if (isitaly == 129 && secretary == 3) {
+    if (layer == 0 || layer == 2) {
+      for (let i = 0; i < list.length; ++i) {
+        if (list[i][0] == 25) list[i][1] -= 2;
+        if (list[i][0] == 210) list[i][1] += 2;
+      }
+    } else if (layer == 1) {
+      for (let i = 0; i < list.length; ++i) {
+        if (list[i][0] == 25) list[i][1] -= 2;
+        if (list[i][0] == 210) list[i][1] += 2;
+      }
+    } else if (layer == 3) {
+      for (let i = 0; i < list.length; ++i) {
+        if (list[i][0] == 25) list[i][1] -= 2;
+        if (list[i][0] == 210) list[i][1] += 2;
       }
     }
   }
